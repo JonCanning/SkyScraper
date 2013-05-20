4c3cec16211b89418292e9607f986313f53b0028
Jon Canning <jon.canning@gmail.com>
2013-05-20 21:35:41 +0100

    Integration test project

---
+++ src/SkyScraper.IntegrationTests/When_running_against_a_real_website.cs
@@ -0,0 +1,24 @@
+using System;
+using System.IO;
+using NUnit.Framework;
+using SkyScraper.Observers.ConsoleWriter;
+using SkyScraper.Observers.ImageScraper;
+
+namespace SkyScraper.IntegrationTests
+{
+    [TestFixture]
+    [Explicit]
+    class When_running_against_a_real_website
+    {
+        [Test]
+        public async void Then_images_should_be_saved()
+        {
+            var scraper = new Scraper(new HttpClient(), new ScrapedUrisDictionary());
+            var io = new ImageScraperObserver(new HttpClient(), new FileWriter(new DirectoryInfo("c:\\temp")));
+            scraper.Subscribe(io);
+            scraper.Subscribe(new ConsoleWriterObserver());
+            scraper.Subscribe(x => Console.WriteLine(x.Uri));
+            await scraper.Scrape(new Uri("http://www.cambridgecupcakes.com/"));
+        }
+    }
+}
