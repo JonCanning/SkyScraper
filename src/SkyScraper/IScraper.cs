ea8592162a276e04761c2a13c66032ea33dd48fe
Jon Canning <jon.canning@gmail.com>
2013-05-23 21:28:48 +0100

    Removed RavenDB

---
+++ src/SkyScraper/IScraper.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SkyScraper
+{
+    public interface IScraper {
+        IDisposable Subscribe(IObserver<HtmlDoc> observer);
+        Task Scrape(Uri uri);
+        List<IObserver<HtmlDoc>> Observers { get; set; }
+    }
+}
