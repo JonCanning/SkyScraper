76215e993741505fc884ed10a36bd14632af3c09
Jon Canning <jon.canning@gmail.com>
2014-10-13 15:44:41 +0100

    update packages and fix tests

--- src/SkyScraper.Tests/ScraperFixtures/When_website_contains_an_explicit_anchor_link.cs
+++ src/SkyScraper.Tests/ScraperFixtures/When_website_contains_an_explicit_anchor_link.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Collections.Generic;
-using System.Net.Http;
 using System.Threading.Tasks;
 using FluentAssertions;
 using NSubstitute;
@@ -8,40 +7,39 @@ using NUnit.Framework;
 
 namespace SkyScraper.Tests.ScraperFixtures
 {
-    [TestFixture]
-    class When_website_contains_an_explicit_anchor_link : ConcernForScraper
-    {
-        readonly List<HtmlDoc> htmlDocs = new List<HtmlDoc>();
-        string page;
+  [TestFixture]
+  class When_website_contains_an_explicit_anchor_link : ConcernForScraper
+  {
+    readonly List<HtmlDoc> htmlDocs = new List<HtmlDoc>();
+    string page;
 
-        protected override void Context()
-        {
-            base.Context();
-            Uri = new Uri("http://test");
-            page = @"<html>
+    protected override void Context()
+    {
+      base.Context();
+      Uri = new Uri("http://test");
+      page = @"<html>
                          <a href=""http://test/#here"">link1</a>
                          </html>";
-            HttpClient.GetString(Uri).Returns(Task.Factory.StartNew(() => page));
-            HttpClient.GetString(Arg.Is<Uri>(x => x != Uri)).Returns(Task.Factory.StartNew(() => { throw new HttpRequestException(); }));
-            OnNext = x => htmlDocs.Add(x);
-        }
-
-        [Test]
-        public void Then_htmldocs_should_contain_home_page()
-        {
-            htmlDocs.Should().Contain(x => x.Uri.ToString() == "http://test/" && x.Html == page);
-        }
-
-        [Test]
-        public void Then_link_should_not_be_scraped()
-        {
-            HttpClient.DidNotReceive().GetString(Arg.Is<Uri>(x => x.ToString() == "http://test/#here"));
-        }
-
-        [Test]
-        public void Then_one_htmldoc_should_be_returned()
-        {
-            htmlDocs.Count.Should().Be(1);
-        }
+      HttpClient.GetString(Uri).Returns(Task.Factory.StartNew(() => page));
+      OnNext = x => htmlDocs.Add(x);
     }
+
+    [Test]
+    public void Then_htmldocs_should_contain_home_page()
+    {
+      htmlDocs.Should().Contain(x => x.Uri.ToString() == "http://test/" && x.Html == page);
+    }
+
+    [Test]
+    public void Then_link_should_not_be_scraped()
+    {
+      HttpClient.DidNotReceive().GetString(Arg.Is<Uri>(x => x.ToString() == "http://test/#here"));
+    }
+
+    [Test]
+    public void Then_one_htmldoc_should_be_returned()
+    {
+      htmlDocs.Count.Should().Be(1);
+    }
+  }
 }
--- src/SkyScraper.Tests/ScraperFixtures/When_website_returns_exception_from_page.cs
+++ src/SkyScraper.Tests/ScraperFixtures/When_website_returns_exception_from_page.cs
@@ -8,59 +8,65 @@ using NUnit.Framework;
 
 namespace SkyScraper.Tests.ScraperFixtures
 {
-    [TestFixture]
-    class When_website_returns_exception_from_page : ConcernForScraper
+  [TestFixture]
+  class When_website_returns_exception_from_page : ConcernForScraper
+  {
+    const string Page = @"<html><a href=""page1"">link1</a></html>";
+    readonly List<HtmlDoc> htmlDocs = new List<HtmlDoc>();
+    bool error;
+    Uri uri;
+
+    protected override void Context()
     {
-        readonly List<HtmlDoc> htmlDocs = new List<HtmlDoc>();
-        const string Page = @"<html><a href=""page1"">link1</a></html>";
-        bool error;
-        Uri uri;
-
-        protected override void Context()
-        {
-            base.Context();
-            Uri = new Uri("http://test");
-            HttpClient.GetString(Uri).Returns(Task.Factory.StartNew(() => Page));
-            HttpClient.GetString(Arg.Is<Uri>(x => x != Uri)).Returns(Task.Run(() => { throw new HttpRequestException(); }));
-            OnNext = x => htmlDocs.Add(x);
-        }
-
-        protected override Scraper CreateClassUnderTest()
-        {
-            SUT = base.CreateClassUnderTest();
-            SUT.OnHttpClientException += delegate { error = true; };
-            SUT.OnScrape += x => uri = x;
-            return SUT;
-        }
-
-        [Test]
-        public void Then_htmldocs_should_contain_home_page()
-        {
-            htmlDocs.Should().Contain(x => x.Uri.ToString() == "http://test/" && x.Html == Page);
-        }
-
-        [Test]
-        public void Then_link_should_be_scraped()
-        {
-            HttpClient.Received().GetString(Arg.Is<Uri>(x => x.ToString() == "http://test/page1"));
-        }
-
-        [Test]
-        public void Then_one_htmldoc_should_be_returned()
-        {
-            htmlDocs.Count.Should().Be(1);
-        }
-
-        [Test]
-        public void Then_error_should_be_true()
-        {
-            error.Should().BeTrue();
-        }
-
-        [Test]
-        public void Then_uri_should_be_set()
-        {
-            uri.ToString().Should().Be("http://test/page1");
-        }
+      base.Context();
+      Uri = new Uri("http://test");
+      HttpClient.GetString(Uri).Returns(Task.Factory.StartNew(() => Page));
+      HttpClient.GetString(Arg.Is<Uri>(x => x != Uri)).Returns(
+        Task.Run(
+          () =>
+          {
+            throw new HttpRequestException();
+            return Page;
+          }));
+      OnNext = x => htmlDocs.Add(x);
     }
+
+    protected override Scraper CreateClassUnderTest()
+    {
+      SUT = base.CreateClassUnderTest();
+      SUT.OnHttpClientException += delegate { error = true; };
+      SUT.OnScrape += x => uri = x;
+      return SUT;
+    }
+
+    [Test]
+    public void Then_htmldocs_should_contain_home_page()
+    {
+      htmlDocs.Should().Contain(x => x.Uri.ToString() == "http://test/" && x.Html == Page);
+    }
+
+    [Test]
+    public void Then_link_should_be_scraped()
+    {
+      HttpClient.Received().GetString(Arg.Is<Uri>(x => x.ToString() == "http://test/page1"));
+    }
+
+    [Test]
+    public void Then_one_htmldoc_should_be_returned()
+    {
+      htmlDocs.Count.Should().Be(1);
+    }
+
+    [Test]
+    public void Then_error_should_be_true()
+    {
+      error.Should().BeTrue();
+    }
+
+    [Test]
+    public void Then_uri_should_be_set()
+    {
+      uri.ToString().Should().Be("http://test/page1");
+    }
+  }
 }
